c5184bcbb9177fd210970d0b42550a4b15e58a17
claes <claes@holmerson.net>
2023-08-17 22:17:43 +0200

    Updated hash

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
             # remeber to bump this hash when your dependencies change.
             #vendorSha256 = pkgs.lib.fakeSha256;
 
-            vendorSha256 = "sha256-LssDoDl50doHpP+sEP0EdMWAFNPb9R7FE75o6Uhe6yY=";
+            vendorSha256 = "sha256-GZLX9cHpjN7UmMqA+v7fizNbMz4sEWJIeKSuMwb8l+c=";
           };
         });
 
